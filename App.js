38f9732d3200edd8b2f6708e37e45483b5da2c37
wilsonericn <wilson.eric.n@gmail.com>
2024-03-21 14:24:35 -0400

    receiving token from Google

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {useState} from "react";
+import {GoogleSignin, GoogleSigninButton} from '@react-native-google-signin/google-signin';
+import {GOOGLE_ANDROID_CLIENT_ID, GOOGLE_IOS_CLIENT_ID, GOOGLE_WEB_CLIENT_ID} from '@env';
+
+GoogleSignin.configure({
+  androidClientId: GOOGLE_ANDROID_CLIENT_ID,
+  iosClientId: GOOGLE_IOS_CLIENT_ID,
+  webClientId: GOOGLE_WEB_CLIENT_ID,
+  scopes: ['profile', 'email'],
+});
+
+const GoogleLogin = async () => {
+  await GoogleSignin.hasPlayServices();
+  return await GoogleSignin.signIn();
+}
 
 export default function App() {
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [signinInProgress, isSigninInProgress] = useState(false);
+  const handleGoogleLogin = async () => {
+    setLoading(true);
+    try {
+      isSigninInProgress(true);
+      const response = await GoogleLogin();
+      isSigninInProgress(false);
+      const { idToken, user } = response;
+      console.log('ID Token', idToken);
+      console.log('User', user);
+
+      // UNCOMMENT WHEN READY TO SEND TO BACK END
+      // if (idToken) {
+      //   const resp = await authAPI.validateToken({
+      //     token: idToken,
+      //     email: user.email,
+      //   });
+      //   await handlePostLoginData(resp.data);
+      // }
+      // DELETE WHEN BACK END IMPLEMENTS SOMETHING LIKE THE FOLLOWING
+      // await axios({
+      //   method: 'get',
+      //   url: `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`,
+      //   withCredentials: true,
+      // });
+    } catch (apiError) {
+      setError(
+          apiError?.response?.data?.error?.message || 'Something went wrong'
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={handleGoogleLogin}
+        disabled={signinInProgress}
+      />
     </View>
   );
+}
+
+async function handleGoogleLogout() {
+  try {
+    await GoogleSignin.signOut();
+  } catch (error) {
+    console.log('Google Sign-Out Error:', error)
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9df',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
